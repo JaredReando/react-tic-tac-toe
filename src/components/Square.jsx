1d05b3be4416ece178f85abcad655a2992a12318
Student <example@example.com>
2019-02-27 14:07:25 -0800

    initial commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+
+
+}
+
+export default Square;
